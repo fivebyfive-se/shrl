856a8a072e7e136ffa24fbdf8e0514cc8d7be586
Kalle Räisänen <revkalle@gmail.com>
2019-12-30 16:07:32 +0100

    progress

--- index.js
+++ index.js
@@ -1,54 +1,96 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
-
+const parse = require('url-parse');
 const Redis = require('ioredis');
+const randomString = require('crypto-random-string');
+
 const redis = new Redis(process.env.REDIS_URL);
 
 const PORT = process.env.PORT || 80;
+const URL_PREFIX = process.env.URL_KEY_PREFIX || 'url_';
 
 const prefixKey = (key) => {
-    return 'url_' + (key || '').replace(/\W+/gi, '');
+    return URL_PREFIX + (key || '').replace(/\W+/gi, '');
 };
-const safeGet = (key, fallback = null) => {
+const safeExists = async (key) => {
     const realKey = prefixKey(key);
-    return redis.get(realKey)
-        .then((res) => res || fallback)
-        .catch(() => fallback);
+    if (realKey === URL_PREFIX) {
+        return false;
+    }
+    try {
+        const exists = await redis.exists(realKey);
+        return !!exists;
+    } catch {
+        return false;
+    }
 };
-const safeSet = (key, value) => {
-    return safeGet(key, null)
-        .then((res) => {
-            if (!res) {
-                return redis.set(prefixKey(key), value);
-            } else {
-                return Promise.resolve(res);
-            }
-        });
+const safeGet = async (key) => {
+    const realKey = prefixKey(key);
+    if (realKey === URL_PREFIX) {
+        return Promise.reject(null);
+    }
+    try {
+        const value = await redis.get(realKey);
+        return value || null;
+    } catch {
+        return Promise.reject(null);
+    }
+};
+const safeSet = async (key, value) => {
+    const keyExists = await safeExists(key);
+
+    if (!keyExists) {
+        const realKey = prefixKey(key);
+        try {
+            const res = await redis.set(realKey, value);
+            return !!res;
+        } catch (e) {
+            return Promise.reject(null);
+        }
+    }
+    return Promise.reject(null);
 };
 
 express()
+    .use(bodyParser.json({type: '*/json'}))
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'pug')
     .get('/', (_, res) => {
-        res.render('index');
+        const suggestion = randomString({length: 6, type: 'url-safe'});
+        res.render('index', { suggestion });
     })
     .get('/url/:key', (req, res) => {
-        safeGet(req.params.key)
-            .then((value) => res.send({ key, value }));
+        safeExists(req.params.key)
+            .then((found) => res.send({ found }))
+            .catch(() => res.status(400));
     })
-    .post('/url/:key/:url', (req, res) => {
-        safeSet(req.params.key, req.params.url)
-            .then((value) => {
-                res.send({ key: req.params.key, value })
-            });
+    .post('/url/:key', (req, res) => {
+        const url = req.body.url || null;
+        safeSet(req.params.key, url)
+            .then(
+                (success) => res.send({ key: req.params.key, url, success }),
+                () => res.status(400)
+            ).catch(() => res.status(400));
     })
     .get('/:key', (req, res) => {
-        redis.get(req.params.key)
-            .then((result) => {
-                res.render('redirect', { url: result });
-            })
-            .catch(() => res.status(400));
+        safeGet(req.params.key)
+            .then(
+                (url) => {
+                    const parsedUrl = parse(url);
+                    console.log(parsedUrl);
+                    let displayUrl = url;
+                    Object.keys(parsedUrl).forEach((k) => {
+                        const val = parsedUrl[k] || '';
+                        if (val.length > 1) {
+                            displayUrl = displayUrl.replace(val, `<span class="url__part--${k}">${val}</span>`)
+                        }
+                    });
+                    res.render('redirect', { url: parsedUrl.href, hostname: parsedUrl.hostname, displayUrl } );
+                },
+                () => res.status(400)
+            ).catch(() => res.status(400));
     })
     .listen(PORT, () => {
         console.log(`Listening on ${PORT}`);
---
+++ public/js/app.js
@@ -0,0 +1,76 @@
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+function safeGetValue(elm, fallback = '') {
+    if (elm) {
+        return elm.value || fallback;
+    }
+    return fallback;
+}
+function replaceClass(elm, replaceClass, replaceWith = null) {
+    if (elm && elm.classList) {
+        elm.classList.remove(replaceClass);
+        if (replaceWith) {
+            elm.classList.add(replaceWith);
+        }
+    }
+}
+
+(() => {
+    const urlField = document.getElementById('url');
+    const keyField = document.getElementById('shortened');
+    const formFieldClass = 'form__field',
+        fieldSuccess = `${formFieldClass}--success`,
+        fieldError = `${formFieldClass}--error`;
+
+    let keyDirty = false;
+
+    if (urlField && keyField) {
+        let lastUrlValue = safeGetValue(urlField);
+        let lastKeyValue = safeGetValue(keyField)
+
+        function urlChanged() {
+            const field = this;
+            const currValue = safeGetValue(field);
+
+            if (lastUrlValue !== currValue && currValue.length > 5) {
+                replaceClass(field, fieldError, fieldSuccess);
+                keyField.style.display = 'block';
+            } else {
+                replaceClass(field, fieldSuccess, fieldError);
+                keyField.style.display = 'none';
+            }
+            lastUrlValue = currValue;
+        }
+        function keyChanged() {
+            const field = this;
+            const currValue = safeGetValue(field);
+
+            if (currValue !== lastKeyValue) {
+                if (!keyDirty) {
+                    keyField.addEventListener('change', keyChanged);
+                    keyDirty = true;
+                }
+                fetch(`/url/${currValue}`).then((resp) => {
+                    
+                });
+            }
+        }
+
+        urlField.addEventListener('keyup', urlChanged);
+        urlField.addEventListener('change', urlChanged);
+        keyField.addEventListener('keyup', keyChanged);
+    }
+})();
