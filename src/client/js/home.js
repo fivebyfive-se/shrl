5a1ab7cef8a167cdf98c6cabcad443ac7215b8d5
Kalle Räisänen <revkalle@gmail.com>
2020-01-01 22:56:53 +0100

    Fixes and refactorings

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -1,5 +1,19 @@
 (() => {
-    const safeGetValue = (elm, fallback = '') => elm ? (elm.value || fallback) : fallback;
+    const formFieldClass = 'form__field',
+        fieldSuccess = `${formFieldClass}--success`,
+        fieldError = `${formFieldClass}--error`,
+        fieldDirty = `${formFieldClass}--dirty`;
+
+    const form = document.getElementById('main-form'),
+        keyContainer = document.getElementById('shortened'),
+        urlField = document.getElementById('url'),
+        keyField = document.getElementById('key'),
+        resultContainer = document.getElementById('result');
+    
+    let keyValid = true,
+        urlValid = false;
+
+    const safeGetValue = (elm, fallback = '') => elm ? (elm.value.replace(/^\s+|\s+$/g, '') || fallback) : fallback;
     const replaceClass = (elm, replaceClass, replaceWith = null) => {
         if (elm && elm.classList) {
             elm.classList.remove(replaceClass);
@@ -8,99 +22,95 @@
             }
         }
     };
+    const validIf = (elm, isValid) => {
+        if (isValid) {
+            replaceClass(elm, fieldError, fieldSuccess);
+        } else {
+            replaceClass(elm, fieldSuccess, fieldError);
+        }
+    };
     const validUrl = (value) => {
         return value && /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
     };
+    const revalidateForm = () => {
+        keyContainer.style.display = urlValid ? 'block' : 'none';
 
-    const form = document.getElementById('main-form');
-    const button = document.getElementById('submit-button');
-    const keyContainer = document.getElementById('shortened');
-    const urlField = document.getElementById('url');
-    const keyField = document.getElementById('key');
-    const resultContainer = document.getElementById('result');
-
-    const formFieldClass = 'form__field',
-        fieldSuccess = `${formFieldClass}--success`,
-        fieldError = `${formFieldClass}--error`;
-
-    let keyDirty = false,
-        formValid = false;
-
-    if (urlField && keyField) {
-        let lastUrlValue = safeGetValue(urlField);
-        let lastKeyValue = safeGetValue(keyField)
-
-        function urlChanged() {
-            const field = this;
-            const currValue = safeGetValue(field);
-            if (lastUrlValue !== currValue) {
-                formValid = validUrl(currValue);
-                if (formValid) {
-                    replaceClass(field, fieldError, fieldSuccess);
-                    form.removeAttribute('disabled');
-                    keyContainer.style.display = 'block';
-                } else {
-                    replaceClass(field, fieldSuccess, fieldError);
-                    form.setAttribute('disabled', 'disabled');
-                    keyContainer.style.display = 'none';
-                }
-                lastUrlValue = currValue;    
-            }
+        if (urlValid && keyValid) {
+            form.removeAttribute('disabled');
+        } else {
+            form.setAttribute('disabled', 'disabled');
         }
+    };
+    const handleChange = (element, eventToWatch, callback) => {
+        ((field, handler, events) => {
+            let lastValue = safeGetValue(field);
 
-        function keyChanged() {
-            const field = this;
-            const currValue = safeGetValue(field);
-
-            if (currValue !== lastKeyValue) {
-                if (!keyDirty) {
-                    keyField.addEventListener('change', keyChanged);
-                    keyDirty = true;
+            function changeHandler() {
+                const currValue = safeGetValue(field);
+                if (currValue !== lastValue) {
+                    if (lastValue === '') {
+                        field.classList.add(fieldDirty);
+                    }
+                    if (handler) {
+                        handler.call(field, currValue);
+                    }
+                    lastValue = currValue;
                 }
-                fetch(`/url/${currValue}`)
-                    .then((resp) => resp.json())
-                    .then((resp) => {
-                        field.classList.remove('error', 'success');
-                        if (resp.found) {
-                            field.classList.add('error');
-                        } else {
-                            field.classList.add('success')
-                        }
-                    });
-            }
-        }
+            };
 
-        function formSubmitted(ev) {
-            ev.preventDefault();
-            if (formValid) {
-                const key = safeGetValue(keyField);
-                const url = safeGetValue(urlField);
+            (events || []).forEach((ev) => field.addEventListener(ev, changeHandler));
+        })(element, callback, Array.isArray(eventToWatch) ? eventToWatch : [eventToWatch || 'change']);
+    };
 
-                form.setAttribute('disabled', 'disabled');
 
-                fetch(`/url/${key}`, {
-                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ url })
-                })
+    handleChange(urlField, ['keyup', 'change'], (currValue) => {
+        urlValid = validUrl(currValue);
+        validIf(urlField, urlValid);
+
+        revalidateForm();
+    });
+
+    handleChange(keyField, ['keyup', 'change'], (currValue) => {
+        if (currValue === '') {
+            keyValid = false;
+            revalidateForm();
+        } else {
+            form.setAttribute('disabled', 'disabled');
+            fetch(`/url/${currValue}`)
                 .then((resp) => resp.json())
                 .then((resp) => {
-                    if (resp.success) {
-                        resultContainer.innerHTML = `<p>Created new short link <a href="/${key}">/${key}</a></p>`;
-                        resultContainer.style.display = 'block';
-                        form.style.display = 'none';
-                    } else {
-                        form.removeAttribute('disabled');
-                    }
-                });
-            }
+                    keyValid = !resp.found;
+                    validIf(keyField, keyValid);
+                    revalidateForm();
+                });    
         }
+    });
 
-        urlField.addEventListener('keyup', urlChanged);
-        urlField.addEventListener('change', urlChanged);
-        keyField.addEventListener('keyup', keyChanged);
+    form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
 
-        form.addEventListener('submit', formSubmitted);
-    }
+        if (keyValid && urlValid) {
+            const key = safeGetValue(keyField);
+            const url = safeGetValue(urlField);
+
+            form.setAttribute('disabled', 'disabled');
+
+            fetch(`/url/${key}`, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ url })
+            })
+            .then((resp) => resp.json())
+            .then((resp) => {
+                if (resp.success) {
+                    resultContainer.innerHTML = `<p>Created new short link <a href="/${key}">/${key}</a></p>`;
+                    resultContainer.style.display = 'block';
+                    form.style.display = 'none';
+                } else {
+                    form.removeAttribute('disabled');
+                }
+            });
+        }
+    });
 })();
