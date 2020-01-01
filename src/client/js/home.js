a91d332f42f6d04efd847dc1d460a797b0988a5e
Kalle Räisänen <revkalle@gmail.com>
2020-01-01 21:23:09 +0100

    progress

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -1,18 +1,4 @@
 (() => {
-    const debounce = (func, wait, immediate) => {
-        let timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
     const safeGetValue = (elm, fallback = '') => elm ? (elm.value || fallback) : fallback;
     const replaceClass = (elm, replaceClass, replaceWith = null) => {
         if (elm && elm.classList) {
@@ -22,14 +8,23 @@
             }
         }
     };
+    const validUrl = (value) => {
+        return value && /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+    };
 
+    const form = document.getElementById('main-form');
+    const button = document.getElementById('submit-button');
+    const keyContainer = document.getElementById('shortened');
     const urlField = document.getElementById('url');
-    const keyField = document.getElementById('shortened');
+    const keyField = document.getElementById('key');
+    const resultContainer = document.getElementById('result');
+
     const formFieldClass = 'form__field',
         fieldSuccess = `${formFieldClass}--success`,
         fieldError = `${formFieldClass}--error`;
 
-    let keyDirty = false;
+    let keyDirty = false,
+        formValid = false;
 
     if (urlField && keyField) {
         let lastUrlValue = safeGetValue(urlField);
@@ -39,17 +34,20 @@
             const field = this;
             const currValue = safeGetValue(field);
             if (lastUrlValue !== currValue) {
-                if (currValue.length > 5) {
+                formValid = validUrl(currValue);
+                if (formValid) {
                     replaceClass(field, fieldError, fieldSuccess);
-                    keyField.style.display = 'block';
+                    form.removeAttribute('disabled');
+                    keyContainer.style.display = 'block';
                 } else {
                     replaceClass(field, fieldSuccess, fieldError);
-                    keyField.style.display = 'none';
+                    form.setAttribute('disabled', 'disabled');
+                    keyContainer.style.display = 'none';
                 }
                 lastUrlValue = currValue;    
             }
         }
-        
+
         function keyChanged() {
             const field = this;
             const currValue = safeGetValue(field);
@@ -59,8 +57,42 @@
                     keyField.addEventListener('change', keyChanged);
                     keyDirty = true;
                 }
-                fetch(`/url/${currValue}`).then((resp) => {
-                    
+                fetch(`/url/${currValue}`)
+                    .then((resp) => resp.json())
+                    .then((resp) => {
+                        field.classList.remove('error', 'success');
+                        if (resp.found) {
+                            field.classList.add('error');
+                        } else {
+                            field.classList.add('success')
+                        }
+                    });
+            }
+        }
+
+        function formSubmitted(ev) {
+            ev.preventDefault();
+            if (formValid) {
+                const key = safeGetValue(keyField);
+                const url = safeGetValue(urlField);
+
+                form.setAttribute('disabled', 'disabled');
+
+                fetch(`/url/${key}`, {
+                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ url })
+                })
+                .then((resp) => resp.json())
+                .then((resp) => {
+                    if (resp.success) {
+                        resultContainer.innerHTML = `<p>Created new short link <a href="/${key}">/${key}</a></p>`;
+                        resultContainer.style.display = 'block';
+                        form.style.display = 'none';
+                    } else {
+                        form.removeAttribute('disabled');
+                    }
                 });
             }
         }
@@ -68,5 +100,7 @@
         urlField.addEventListener('keyup', urlChanged);
         urlField.addEventListener('change', urlChanged);
         keyField.addEventListener('keyup', keyChanged);
+
+        form.addEventListener('submit', formSubmitted);
     }
 })();
