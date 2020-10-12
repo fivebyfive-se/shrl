3d503d3ae01b0b02688db4acf3918c7f5721978d
Kalle R <kr@impera.se>
2020-10-12 19:44:36 +0200

    Add pricing levels + mod how prismic rendering is done

--- server/lib/session.js
+++ server/lib/session.js
@@ -1,12 +1,12 @@
-
+require('dotenv').config();
 const expressSession = require('express-session');
 const Redis = require('ioredis');
 
 let RedisStore = require('connect-redis')(expressSession);
-let client = new Redis(process.env.REDIS_URL);
+let client = new Redis(process.env.REDIS_URL, { connectTimeout: 30000, maxRetriesPerRequest: 4 });
 
 module.exports = {
-    store: new RedisStore({ client }),
+    store: process.env.NODE_ENV === 'production' ? new RedisStore({ client }) : null,
     secret: process.env.SESSION_SECRET || 'shirrley',
     cookie: {
         secure: process.env.NODE_ENV === 'production',
