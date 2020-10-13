c5aaf048071580700091a6504c98c6b8b089c03f
Kalle R <kr@impera.se>
2020-10-13 02:59:03 +0200

    Ooops! typo

--- server/lib/session.js
+++ server/lib/session.js
@@ -5,7 +5,7 @@ const Redis = require('ioredis');
 let RedisStore = require('connect-redis')(expressSession);
 let client = new Redis(process.env.REDIS_URL, { connectTimeout: 30000 });
 
-session.on('error', (...args) => console.log(args));
+client.on('error', (...args) => console.log(args));
 
 module.exports = {
     store: process.env.NODE_ENV === 'production' ? new RedisStore({ client }) : null,
